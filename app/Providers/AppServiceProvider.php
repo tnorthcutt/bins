68130b3b526e72d29a40e946f2ecf7d60df2c0f6
Travis Northcutt <travis@travisnorthcutt.com>
2020-02-08 21:40:19 -0700

    add use statement

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\TrashBinMailbox;
 use BeyondCode\Mailbox\Facades\Mailbox;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
