5eff66d0e6b240260f908274ab04bebf14d94bf5
Travis Northcutt <travis@travisnorthcutt.com>
2020-02-17 14:37:24 -0700

    more strict pattern matching

--- app/TrashBinMailbox.php
+++ app/TrashBinMailbox.php
@@ -12,10 +12,10 @@ class TrashBinMailbox
 
     public function __invoke(InboundEmail $email)
     {
-        if (strpos($email->text(), '*Trash*') !== false) {
+        if (strpos($email->html(), '<strong>Trash*') !== false) {
             $this->status = 'bin-status trash';
         }
-        if (strpos($email->text(), '*Recycling*') !== false) {
+        if (strpos($email->html(), '<strong>Recycling') !== false) {
             $this->status = 'bin-status both';
         }
         Log::info('Status: ' . $this->status);
